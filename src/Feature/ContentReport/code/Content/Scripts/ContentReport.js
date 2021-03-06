891b60f774669d02b1549daeedf42f197f830895
Sayak Dawn <sayak.dawn@hotmail.com>
2021-03-06 14:16:31 +1100

    Datepicker added without style

--- src/Feature/ContentReport/code/Content/Scripts/ContentReport.js
+++ src/Feature/ContentReport/code/Content/Scripts/ContentReport.js
@@ -1,1 +1,13 @@
-﻿
+﻿$(document).ready(function () {
+
+    // Datepicker initialisation
+    var options = {
+        format: 'mm/dd/yyyy',
+        todayHighlight: true,
+        autoclose: true
+    }
+    $('#fromdate').datepicker(options);
+    $('#todate').datepicker(options);
+
+
+});
