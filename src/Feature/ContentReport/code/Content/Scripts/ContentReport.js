9ad0ac6630cacb47f09f734930afed54d3b44818
sayakdawn <sayak.dawn@hotmail.com>
2021-03-06 16:50:42 +1100

    Tab and datatable

--- src/Feature/ContentReport/code/Content/Scripts/ContentReport.js
+++ src/Feature/ContentReport/code/Content/Scripts/ContentReport.js
@@ -2,12 +2,20 @@
 
     // Datepicker initialisation
     var options = {
-        format: 'mm/dd/yyyy',
+        format: 'dd/mm/yyyy',
         todayHighlight: true,
         autoclose: true
     }
     $('#fromdate').datepicker(options);
     $('#todate').datepicker(options);
 
+
+    // Tab Data tables    
+    $('#dt-tab-*').DataTable({
+        "paging": true,
+        "ordering": true,
+        "pagingType": "full_numbers"
+    });
+    //$('.dataTables_length').addClass('bs-select');
 
 });
