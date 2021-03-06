2c5ee23d5d5a21573e83dec515aca8451ba2f765
sayakdawn <sayak.dawn@hotmail.com>
2021-03-07 10:21:56 +1100

    area register

---
+++ src/Feature/ContentReport/code/App_Start/RouteConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SitecoreDiser.Feature.ContentReport.App_Start
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+            routes.MapRoute(name: "ContentReport_default", url: "{controller}/{action}/{id}", defaults: new { controller = "ContentReport", action = "ContentReport", id = UrlParameter.Optional });
+        }
+    }
+}
---
+++ src/Feature/ContentReport/code/Areas/ContentReportAreaRegistration.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace SitecoreDiser.Feature.ContentReport.Areas
+{
+    public class ContentReportAreaRegistration : AreaRegistration
+    {
+        public override string AreaName { get { return "ContentReport"; } }
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute("ContentReport_default", "ContentReport/{controller}/{action}/{id}", new { action = "ContentReport", id = UrlParameter.Optional });
+        }
+    }
+}
