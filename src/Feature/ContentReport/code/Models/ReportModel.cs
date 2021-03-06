095c084d65ac816e0323a455eb2faf8aa4d218a0
Mahesh Raghupathi <maheshraghupathi@gmail.com>
2021-03-06 23:05:57 +1100

    updated date format in model

--- src/Feature/ContentReport/code/Models/ReportModel.cs
+++ src/Feature/ContentReport/code/Models/ReportModel.cs
@@ -9,9 +9,9 @@ namespace SitecoreDiser.Feature.ContentReport.Models
     public class ReportModel
     {
         public string StartDate { get; set; }
-        public DateTime? StartDateTime => string.IsNullOrWhiteSpace(StartDate) ? (DateTime?)null : DateTime.ParseExact(StartDate, "d MMM yyyy", CultureInfo.InvariantCulture).ToUniversalTime();
+        public DateTime? StartDateTime => string.IsNullOrWhiteSpace(StartDate) ? (DateTime?)null : Convert.ToDateTime(StartDate).ToUniversalTime();
         public string EndDate { get; set; }
-        public DateTime? EndDateTime => string.IsNullOrWhiteSpace(EndDate) ? (DateTime?)null : DateTime.ParseExact(EndDate, "d MMM yyyy", CultureInfo.InvariantCulture).AddDays(1).ToUniversalTime();
+        public DateTime? EndDateTime => string.IsNullOrWhiteSpace(EndDate) ? (DateTime?)null : Convert.ToDateTime(EndDate).AddDays(1).ToUniversalTime();
         public string Type { get; set; }
         public int Page { get; set; }
         public int NoOfItems { get; set; }
